693edbaa411b98c427b13ea3552bdff96236fe53
Jason Kao <jason.kao@columbia.edu>
2021-04-11 13:42:55 -0400

    print header of first file

--- cut.py
+++ cut.py
@@ -7,21 +7,29 @@ with open(sys.argv[1]) as f:
 
 filenames = sys.argv[2:]
 
+
 def filter_file(fname):
-     date = "2021-" + "-".join(fname.split("/")[1:-2])
-     output = ""
-     with open(fname) as f:
-          for line in f:
-               if line[:19] in placekeys:
-                    output += line[:-1] + "," + date + "\n"
-     return output
+    date = "2021-" + "-".join(fname.split("/")[1:-2])
+    output = ""
+    with open(fname) as f:
+        for line in f:
+            if line[:19] in placekeys:
+                output += line[:-1] + "," + date + "\n"
+    return output
+
 
 def main():
-     pool = Pool()
-     for output in pool.imap(filter_file, filenames):
-          sys.stdout.write(output)
-     pool.close()
-     pool.join()
+    # Print header of first file
+    with open(filenames[0]) as f:
+        sys.stdout.write(f.readline()[:-1] + ',"date"\n')
 
-if __name__ == '__main__':
-     main()
+    # Process the files
+    pool = Pool()
+    for output in pool.imap(filter_file, filenames):
+        sys.stdout.write(output)
+    pool.close()
+    pool.join()
+
+
+if __name__ == "__main__":
+    main()
