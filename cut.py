e9a1a2495025ebe5262d8edee0d880bc1334c9b5
Jason Kao <jason.kao@columbia.edu>
2021-04-11 13:24:32 -0400

    multiprocessing doubled the speed of cut.py

--- cut.py
+++ cut.py
@@ -1,17 +1,32 @@
+from multiprocessing import Pool
 import sys
 import json
 
 with open(sys.argv[1]) as f:
     placekeys = set([f["properties"]["placekey"] for f in json.load(f)["features"]])
 
-is_first = True
-for fname in sys.argv[2:]:
-    date = "2021-" + "-".join(fname.split("/")[1:-2])
-    with open(fname) as f:
-        if is_first:
-            sys.stdout.write(next(f)[:-1] + ',"date"\n')
-            is_first = False
-        for line in f:
-            if line[:19] in placekeys:
-                sys.stdout.write(line[:-1] + "," + date + "\n")
-        print("Completed", fname, file=sys.stderr)
+filenames = sys.argv[2:]
+
+def filter_file(fname):
+     date = "2021-" + "-".join(fname.split("/")[1:-2])
+     output = ""
+     # is_first = True
+     with open(fname) as f:
+        # if is_first:
+        #     output += next(f)[:-1] + ',"date"\n'
+        #     is_first = False
+          for line in f:
+               if line[:19] in placekeys:
+                    output += line[:-1] + "," + date + "\n"
+        # print("Completed", fname, file=sys.stderr)
+     return output
+
+def main():
+     pool = Pool()
+     for output in pool.imap(filter_file, filenames):
+          sys.stdout.write(output)
+     pool.close()
+     pool.join()
+
+if __name__ == '__main__':
+     main()
